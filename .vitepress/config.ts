b2b324cbed9afae4bfbf9de98e99d3eff2156dbc
Jacob Hummer <jcbhmr@outlook.com>
2024-01-09 22:42:34 +0000

    icons

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,10 +2,38 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  // Surprised this isn't the default. 🤷‍♂️
+  srcExclude: ["**/README.md", "**/CONTRIBUTING.md"],
+
+  // https://vitejs.dev/config/
+  vite: {},
+
+  // https://vitepress.dev/guide/sitemap-generation
+  sitemap: {
+    hostname: "https://1brc.dev",
+  },
+
   title: "1BRC",
   description: "1 Billion Row Challenge",
+
+  head: [
+    // https://github.com/vuejs/vitepress/issues/560
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel#license
+    [
+      "link",
+      {
+        rel: "license",
+        href: "https://github.com/1brc/1brc.github.io/blob/main/LICENSE",
+      },
+    ],
+  ],
+
+  // https://vitepress.dev/reference/default-theme-config
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
+    logo: "/logo.png",
+
     nav: [
       // { text: 'Home', link: '/' },
       // { text: 'Examples', link: '/markdown-examples' }
@@ -24,5 +52,17 @@ export default defineConfig({
     socialLinks: [
       { icon: "github", link: "https://github.com/1brc/1brc.github.io" },
     ],
+
+    // https://vitepress.dev/reference/default-theme-edit-link
+    editLink: {
+      pattern: "https://github.com/1brc/1brc.github.io/edit/main/:path",
+    },
+
+    // https://github.com/vuejs/vitepress/issues/1037
+    footer: {
+      message:
+        'Released under the <a href="https://github.com/1brc/1brc.github.io/blob/main/LICENSE">0BSD License</a>.',
+      copyright: "Copyright © 2023 The One Billion Row Challenge",
+    },
   },
 });
